f15c893fc76c6733ee568e835972ee812191472d
Tharun <tharungowda30115@gmail.com>
2025-02-07 19:45:12 +0530

    app.js

--- app.js
+++ app.js
@@ -6,3 +6,9 @@ const navToggler = document.querySelector("[data-nav-toggler]");
 navToggler.addEventListener("click", function () {
   navbar.classList.toggle("active");
 });
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  header.classList[this.scrollY > 50 ? "add" : "remove"]("active");
+});
